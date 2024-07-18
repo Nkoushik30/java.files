0b6ac84a316c12ee0fa11590d4dc7ff3a8b0a239
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-07-18 12:22:46 +0530

    Create file.java

---
+++ file.java
@@ -0,0 +1,42 @@
+//hello world!! program.java
+import java.util.*;
+class HelloWorld
+{
+public static void main(String args[])
+{
+
+System.out.println("Hello World!!!");
+}
+}
+//local variables.java
+import java.util.*;
+class lvgv
+{
+public static void main(String args[])
+{
+int a=10;
+System.out.println("the number is"+a);
+}
+}
+//addition.java
+import java.util.*;
+class add
+{
+public static void main(String args[])
+{
+ int a=50;
+ int b=60;
+ System.out.println(a+b);
+ }
+ }
+ //subtraction.java
+ import java.util.*;
+class sub
+{
+public static void main(String args[])
+{
+int a=15,b=20;
+System.out.println(a-b);
+}
+}
+//*thank you*//
