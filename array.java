4a20acbd5e5698e95260aafcdc1756194b26c4d1
koushik_nelluri <160224734+Nkoushik30@users.noreply.github.com>
2024-08-10 12:04:46 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+class arry
+{
+  public static void main(String []args)
+  { 
+    int arr[]={11,10,22,3,4,5,7,6};
+    int sum=0;
+    for(int i:arr){
+    sum+=i;
+    }
+    System.out.println(sum);
+    }
+    }
+//
+import java.util.*;
+class Argc
+{
+  public static void main(String []args)
+   {
+    System.out.println(args[0]);
+       System.out.println(args[1]);
+          System.out.println(args[2]);
+     }
+  }
